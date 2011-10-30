7329805a1a6d123ab77a7a58de3cb45b6cc5a56c
Simon Goudie <simon@Rexs-MacBook-Pro.local>
2011-10-30 20:09:43 +1100

    Initial Commit

---
+++ LinkedReverser/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LinkedReverser
+//
+//  Created by Simon Goudie on 30/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
